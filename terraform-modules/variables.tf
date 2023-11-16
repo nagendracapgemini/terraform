34c555eba2791d6996c6c7da60e3ce7629317d3a
nagendracapgemini <144872560+nagendracapgemini@users.noreply.github.com>
2023-11-16 14:47:01 +0530

    Update variables.tf

--- terraform-modules/variables.tf
+++ terraform-modules/variables.tf
@@ -1,11 +1,13 @@
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
+  default     = UDP
 }
 
 variable "location" {
   description = "Azure region for resources"
   type        = string
+  default     = eastus
 }
 variable "subnet_id" {
   description = "Azure region for resources"
